8c913db96df7df84dfe5117b3ab1dad0be78d41b
BeleRohit <bele.12015904@lpu.in>
2026-05-02 17:10:58 +0530

    Fix TemplateResponse signature

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/health")
 def health_check():
